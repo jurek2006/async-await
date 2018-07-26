b5deec0b1565d63c82f0382ef3ff567ccc08268f
Jurek <jurek2006@gmail.com>
2018-07-26 13:41:39 +0200

    Podstawowy async/await

--- app-promises.js
+++ app-promises.js
@@ -57,9 +57,26 @@
     });
  };
 
+ const getStatusAlt = async (userId) => {
+    const user = await getUser(userId);
+    const grades = await getGrades(user.schoolId);
+    let average = 0;
+        
+    if(grades.length > 0){
+        average = grades.map(grade => grade.grade).reduce((a, b) => a + b) / grades.length;
+    }
+    
+    return `${user.name} has ${average}% in the class.`;
+ };
 
- getStatus(2).then(status => {
-     console.log(status);
- }).catch(err => {
-    console.log(err);
- });
+
+ getStatusAlt(1).then(status => {
+    console.log(status);
+ }).catch(err => console.log(err));
+
+
+//  getStatus(1).then(status => {
+//      console.log(status);
+//  }).catch(err => {
+//     console.log(err);
+//  });
